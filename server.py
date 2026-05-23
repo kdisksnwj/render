0ebb5424cbc616a4812f0b7ef921654e06afb8a3
kdisksnwj <maximeriopel1234@gmail.com>
2026-05-23 09:46:36 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,12 +1,29 @@
 from flask import Flask, request, jsonify
+import time
 
 app = Flask(__name__)
 
+connections = []
+
 @app.route("/")
 def home():
-    return "Serveur Render en ligne"
+    return "Serveur en ligne"
 
 @app.route("/message", methods=["POST"])
 def message():
     data = request.json
-    return jsonify({"reponse": data.get("message", "")})
+
+    entry = {
+        "message": data.get("message"),
+        "time": time.strftime("%H:%M:%S")
+    }
+
+    connections.append(entry)
+
+    print("CONNEXION :", entry)
+
+    return jsonify({"reponse": data.get("message")})
+
+@app.route("/logs")
+def logs():
+    return jsonify(connections)
