067111db4f3e2175acb3ceac4afa56460a152775
kdisksnwj <maximeriopel1234@gmail.com>
2026-05-23 09:39:58 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,12 @@
+from flask import Flask, request, jsonify
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Serveur Render en ligne"
+
+@app.route("/message", methods=["POST"])
+def message():
+    data = request.json
+    return jsonify({"reponse": data.get("message", "")})
