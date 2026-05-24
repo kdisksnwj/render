18f8f0868d4b74ab971820182c6c8f4cbac1eda8
kdisksnwj <maximeriopel1234@gmail.com>
2026-05-24 12:55:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,6 @@ def send():
 def data():
     return jsonify(data_store)
 
-# IMPORTANT POUR RENDER
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
